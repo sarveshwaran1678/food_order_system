37bd75f7e9bd18d75cb4674feb4bd92637b68ffa
sarveshwaran1678 <saruguru008@gmail.com>
2023-12-10 23:11:07 +0530

    something

--- account/admin-page.php
+++ account/admin-page.php
@@ -44,6 +44,13 @@ include 'includes/connect.php';
   box-sizing: border-box;
 }
 
+input[type="text"], input[type="number"] {
+  margin: 0px;
+}
+
+.row .column {
+  padding : 0;
+}
 .input-field:focus {
   outline: none;
   border-color: #3c763d;
@@ -125,7 +132,7 @@ input[type="number"] {
             <nav class="navbar-color">
                 <div class="nav-wrapper">
                     <ul class="left">                      
-                      <li><h1 class="logo-wrapper"><a href="index.php" class="brand-logo darken-1"><img src="images/materialize-logo.png" alt="logo"></a> <span class="logo-text">Logo</span></h1></li>
+                      <li><h1 class="logo-wrapper"><a href="index.php" class="brand-logo darken-1"><img src="images/logo.png" alt="logo"></a> <span class="logo-text">Logo</span></h1></li>
                     </ul>
                 </div>
             </nav>
