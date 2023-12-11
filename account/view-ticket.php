dc8ad8025a006ab62e6848c6beb08b5bde605922
lakshitha Hewagamage <lsmakandura@gmail.com>
2023-12-11 11:43:02 +0530

    fdgh

--- account/view-ticket.php
+++ account/view-ticket.php
@@ -316,7 +316,8 @@ if($continue){
   <footer class="page-footer">
     <div class="footer-copyright">
       <div class="container">
-        <span>Copyright © 2023 <a class="grey-text text-lighten-4" href="#" target="_blank">Students</a> All rights reserved.</span>
+        <span>Copyright © 2023
+           <a class="grey-text text-lighten-4" href="#" target="_blank">Students</a> All rights reserved.</span>
         <span class="right"> Design and Developed by <a class="grey-text text-lighten-4" href="#">Students</a></span>
         </div>
     </div>
